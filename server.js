1f2eead265264d0e82d1de9965449553600bc24f
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-11-29 16:57:22 -0800

    Page has loaded

--- server.js
+++ server.js
@@ -2,23 +2,32 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express();
 const PORT = process.env.PORT || 4000;
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
+
 
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/workout',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
     }
   );
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
 
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, ".//Develop/public/exercise.html"));
+});
 
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Develop/public/stats.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`)
